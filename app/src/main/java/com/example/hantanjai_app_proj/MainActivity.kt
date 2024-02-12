f43b14deb14de4a0de0ebaa49ca0328239691383
PattheeraTee <pattheera.mwny@gmail.com>
2024-02-13 04:11:27 +0700

    create nav bar

--- app/src/main/java/com/example/hantanjai_app_proj/MainActivity.kt
+++ app/src/main/java/com/example/hantanjai_app_proj/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.hantanjai_app_proj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.hantanjai_app_proj.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(Home())
+                R.id.notification -> replaceFragment(Notification())
+                R.id.other -> replaceFragment(Other())
+                R.id.pay_bill -> replaceFragment(Paybill())
+
+                else -> {
+
+                }
+
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransition = fragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.frame_layout,fragment)
+        fragmentTransition.commit()
     }
 }
+
