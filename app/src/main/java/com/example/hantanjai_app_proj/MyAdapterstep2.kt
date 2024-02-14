fbe665835797d8b8b469040b09ed10194af1dce2
PattheeraTee <pattheera.mwny@gmail.com>
2024-02-15 00:51:47 +0700

    commit practice get value edittext

--- app/src/main/java/com/example/hantanjai_app_proj/MyAdapterstep2.kt
+++ app/src/main/java/com/example/hantanjai_app_proj/MyAdapterstep2.kt
@@ -1,30 +1,53 @@
 package com.example.hantanjai_app_proj
 
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import de.hdodenhof.circleimageview.CircleImageView
 
-class MyAdapterstep2(val items:Array<String>, val imageId: IntArray):
-    RecyclerView.Adapter<MyAdapterstep2.ViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapterstep2.ViewHolder {
+class MyAdapterstep2(
+    private val friendNames: Array<String>,
+    private val userProfiles: IntArray,
+    private val editTextValues: MutableList<String?>
+) : RecyclerView.Adapter<MyAdapterstep2.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(
-            R.layout.todo_taskedit, parent, false))
+                R.layout.todo_taskedit, parent, false
+            )
+        )
     }
-    override fun onBindViewHolder(holder: MyAdapterstep2.ViewHolder, position: Int)
-    {
-        holder.nameTextView?.text = items.get(position)
-        holder.imageIdView?.setImageResource(imageId.get(position))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.nameTextView?.text = friendNames[position]
+        holder.imageIdView?.setImageResource(userProfiles[position])
+
+        holder.editText!!.setOnKeyListener { _, keyCode, _ ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                editTextValues[position] = holder!!.editText!!.text.toString()
+                true
+            } else {
+                false
+            }
+        }
     }
+
     override fun getItemCount(): Int {
-        return items.size
+        return friendNames.size
+    }
+
+    fun getEditTextValues(): List<String?> {
+        return editTextValues
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        internal var nameTextView : TextView? = itemView.findViewById(R.id.task_titlestep2)
-        internal var imageIdView : CircleImageView? = itemView.findViewById(R.id.profile_imagestep2)
+        internal var nameTextView: TextView? = itemView.findViewById(R.id.task_titlestep2)
+        internal var imageIdView: CircleImageView? = itemView.findViewById(R.id.profile_imagestep2)
+        internal var editText: EditText? = itemView.findViewById(R.id.task_editstep2)
     }
 }
