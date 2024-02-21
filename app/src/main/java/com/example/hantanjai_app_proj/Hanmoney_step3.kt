37edcc442808ffc01114a1b98533f76269641c93
PattheeraTee <pattheera.mwny@gmail.com>
2024-02-22 00:42:11 +0700

    create funciton hanmoney

--- app/src/main/java/com/example/hantanjai_app_proj/Hanmoney_step3.kt
+++ app/src/main/java/com/example/hantanjai_app_proj/Hanmoney_step3.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Hanmoney_step3 : AppCompatActivity() {
+//    comment first
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hanmoney_step3)
